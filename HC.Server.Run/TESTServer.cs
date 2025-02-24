e87ea9469b94559fe7157582f73ed6abb76941c5
Canaban <1225842905@qq.com>
2025-02-24 17:55:28 +0800

    init code

---
+++ HC.Server.Run/TESTServer.cs
@@ -0,0 +1,37 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HC.Server.Run
+{
+    public class TESTServer : BaseLogger, IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            TESTRun();
+            await Task.CompletedTask;
+        }
+
+        public void TESTRun()
+        {
+            logger.Debug("TESTRun开始");
+            Thread.Sleep(2000);
+            logger.Debug("TESTRun停止2秒");
+            logger.Debug("TESTRun结束");
+            logger.Debug("====================================================================");
+            logger.Info("TESTRun开始");
+            Thread.Sleep(2000);
+            logger.Info("TESTRun停止2秒");
+            logger.Info("TESTRun结束");
+            logger.Info("====================================================================");
+            logger.Error("TESTRun开始");
+            Thread.Sleep(2000);
+            logger.Error("TESTRun停止2秒");
+            logger.Error("TESTRun结束");
+            logger.Error("====================================================================");
+        }
+    }
+}
